f96bb6928599c1b109e334db83a67bbb863d6944
sunil.hogtapur <sunil.hogtapur@ltts.com>
2022-11-03 16:40:39 +0530

    Updates

---
+++ static/js/632.4eed8146.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkoot_app=self.webpackChunkoot_app||[]).push([[632],{632:function(p,e,t){t.r(e),e.default={}}}]);
+//# sourceMappingURL=632.4eed8146.chunk.js.map
---
+++ static/js/850.02afb5a9.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkoot_app=self.webpackChunkoot_app||[]).push([[850],{850:function(p,e,t){t.r(e),e.default={}}}]);
+//# sourceMappingURL=850.02afb5a9.chunk.js.map
